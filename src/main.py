9661d48cc33f0fbfa036de5e4817f4384f8efbc3
tiaszz <iskiizeh3@gmail.com>
2024-11-14 00:57:52 -0300

    procedure generation map

--- src/main.py
+++ src/main.py
@@ -2,8 +2,8 @@ import tcod
 
 from engine import Engine
 from entity import Entity
-from game_map import GameMap
 from input_handler import EventHandler
+from procgen import generate_dungeon
 
 
 def main():
@@ -23,7 +23,7 @@ def main():
     npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "@", (255, 255, 0))
     entities = {npc, player}
 
-    game_map = GameMap(map_width, map_height)
+    game_map = generate_dungeon(map_width, map_height)
 
     engine = Engine(
         entities=entities, event_handler=event_handler, game_map=game_map, player=player
